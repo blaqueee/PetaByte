253581da05e6e7fb8370e58635af4f273ea46f71
blaqueee <kajrattobokelov2@gmail.com>
2023-06-20 20:13:50 +0600

    init

---
+++ src/main/java/com/spring/petabyte/PetabyteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.petabyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetabyteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetabyteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/petabyte/PetabyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.petabyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetabyteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
